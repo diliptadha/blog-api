115bc413be754610a5d1358acb31d58c69edd3fd
parul university <diliptadha.dev@gmail.com>
2025-02-14 10:03:54 +0530

    remove temp data in blog data

--- src/blogData.js
+++ src/blogData.js
@@ -1,22 +1,3 @@
-let blogs = [
-  {
-    id: "b916b300-17ce-47c6-8082-dbf1431a31cc",
-    title: "diasdf",
-    content: "asdf",
-    comments: ["comment1", "comment2"],
-  },
-  {
-    id: "876a0e1c-2101-44b3-9cba-b56d32a3b12c",
-    title: "diasdf",
-    content: "asdf",
-    comments: [],
-  },
-  {
-    id: "80f38d83-868e-43c1-b23b-9676adf3b844",
-    title: "diasdf",
-    content: "asdf",
-    comments: ["comment1"],
-  },
-];
+let blogs = [];
 
 export default blogs;
